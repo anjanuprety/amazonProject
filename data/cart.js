4f44b8c7232d7ed48522585e150e6dd1fb2a74f7
Anjan Upreti <anjanuprety787@gmail.com>
2024-09-24 20:24:13 +0545

    cleaning the code further through modules

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart=[]; 
+
+export const addtoCart=(productId)=>{ 
+  let matchingItem;
+
+      cart.forEach((cartItem)=>{
+        if(cartItem.productId===productId){ 
+          matchingItem=cartItem; //checking if the product is already in the cart
+        }
+      });
+      if(matchingItem){
+        matchingItem.quantity++; //if the product is already in the cart, increase the quantity
+      }
+      else{
+        cart.push({
+          productId: productId,  //if the product is not in the cart, add it to the cart
+          quantity: 1
+        });
+      };
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js"; //.. means go up one level in the directory structure
+import {cart,addtoCart} from "../data/cart.js"; //.. means go up one level in the directory structure
 import {products} from "../data/products.js";
 let productsHTML=""; //creating a variable to store the html of all the products
 
@@ -57,34 +57,21 @@ products.forEach((product) => {
   `; //generating html for each product and storing it in a variable called html 
 });
 
+const updateCartQuantity=()=>{
+  let cartQuantity=0;
+  cart.forEach((item)=>{
+    cartQuantity+=item.quantity; //calculating the total quantity of items in the cart
+  });
+  document.querySelector('.js-cart-quantity').textContent=cartQuantity; //displaying the total quantity of items in the cart
+}
+
+
 document.querySelector(".js-products-grid").innerHTML = productsHTML; //displaying the html of all the products in the products grid
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId= button.dataset.productId; //getting the product name from the data attribute of the button
-      let matchingItem;
-
-      cart.forEach((item)=>{
-        if(item.productId===productId){ 
-          matchingItem=item; //checking if the product is already in the cart
-        }
-      });
-      if(matchingItem){
-        matchingItem.quantity++; //if the product is already in the cart, increase the quantity
-      }
-      else{
-        cart.push({
-          productId: productId,  //if the product is not in the cart, add it to the cart
-          quantity: 1
-        });
-      };
-
-      let cartQuantity=0;
-        cart.forEach((item)=>{
-          cartQuantity+=item.quantity; //calculating the total quantity of items in the cart
-        });
-
-        document.querySelector('.js-cart-quantity').textContent=cartQuantity; //displaying the total quantity of items in the cart
-
+      addtoCart(productId);
+      updateCartQuantity();
     }); 
   });
